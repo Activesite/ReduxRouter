f48d915043a31b390582ba67c9a7f191135d2750
Saran Yamasathien <pom.saran@gmail.com>
2017-06-17 10:50:28 +0700

    Lecture 131

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 import React,{Component} from 'react';
 import { connect } from 'react-redux';
 import {fetchPosts} from '../actions';
-
+import {Link} from 'react-router-dom';
 class PostsIndex extends Component {
     componentDidMount(){
         this.props.fetchPosts();
@@ -21,6 +21,11 @@ class PostsIndex extends Component {
         console.log(this.props.posts);
         return(
             <div>
+                <div className="text-xs-right">
+                    <Link className="btn btn-primary" to="/posts/new">
+                        Add a Posts
+                    </Link>
+                </div>
                 <h3>Posts</h3>
                 <ul className="list-group">
                     {this.renderPosts()}
